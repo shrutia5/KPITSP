eb980bf1487d53da4f2b21efcac83d0b90888a51
mkviDev <kiran.malave@mkvisolutions.com>
2022-07-22 16:30:43 +0530

    Chat Message timestamp

--- application/controllers/Messages.php
+++ application/controllers/Messages.php
@@ -75,16 +75,49 @@ class Messages extends CI_Controller {
                 // student sent message to admin
                 $data['by_admin'] = 'n';
             }
-            
+            date_default_timezone_set('Asia/Kolkata');
             $data['message'] = $this->input->post('message');
             $data['project_id'] = $this->input->post('project_id');
-                $data['created_date'] = date("Y-m-d h:i:s");
+                $data['created_date'] = date("Y-m-d H:i:s");
                 $issave = $this->CommonModel->saveMasterDetails("project_messages",$data);
                 if($issave){
                     // send notification to user
                     if($userType == "Admin"){
-                        //$this->notification($reciever);
+
+                        // message to team leader
+                        $join = array();
+                        $join[0]['type'] ="LEFT JOIN";
+                        $join[0]['table']="userregistration";
+                        $join[0]['alias'] ="m";
+                        $join[0]['key1'] ="userID";
+                        $join[0]['key2'] ="userID";
+                        $whereProject = array("projectID="=>$data['project_id']);
+                        $select1="m.firstname,m.lastName,m.email";
+                        $membersQ = $this->CommonModel->GetMasterListDetails($select1,'project_master',$whereProject,'','',$join,'');
+                        if(!empty($membersQ)){
+                            foreach($membersQ as $member){
+                                $this->notification($member->firstname, $member->email);
+                            }
+                        }
+
+                        // message to team member
+                        $join = array();
+                        $join[0]['type'] ="LEFT JOIN";
+                        $join[0]['table']="userregistration";
+                        $join[0]['alias'] ="m";
+                        $join[0]['key1'] ="memberID";
+                        $join[0]['key2'] ="userID";
+                        $whereProject = array("projectID="=>$data['project_id']);
+                        $select1="m.firstname,m.lastName,m.email";
+                        $membersQ = $this->CommonModel->GetMasterListDetails($select1,'membersdetails',$whereProject,'','',$join,'');
+                        if(!empty($membersQ)){
+                            foreach($membersQ as $member){
+                                $this->notification($member->firstname, $member->email);
+                            }
+                        }
+                        
                     }
+                    $status['userType'] = $userType; 
                     $status['msg'] = ''.date("h:i a"); 
                     $status['statusCode'] = 200;
                     $status['flag'] = 'S';
@@ -103,13 +136,29 @@ class Messages extends CI_Controller {
 	}
     public function read($sender,$reciever=''){
 
+        $lastReadMsgId = intval($this->input->post('lastReadMsgId'));
+        if(!empty($lastReadMsgId)){
+            
+            $projectID = intval($this->input->post('msgProjectID'));
+            $whereMsg = array("userID"=>$sender, "projectID"=>$projectID);
+
+            $msgDetails = $this->CommonModel->getMasterDetails('project_messages_read_stats','',$whereMsg);
+            
+            if(!empty($msgDetails)){
+                $this->CommonModel->updateMasterDetails("project_messages_read_stats",array("lastReadMsgId"=>$lastReadMsgId), $whereMsg);
+            }else{
+                $this->CommonModel->saveMasterDetails("project_messages_read_stats",array("lastReadMsgId"=>$lastReadMsgId, "userID"=>$sender, "projectID"=>$projectID));
+            }
+        }
+        
+
         $where = array("userID"=>$this->session->userdata('userId'));
         $user = $this->CommonModel->getMasterDetails('userregistration','',$where);
         if(isset($user) && !empty($user)){
             $data = array();
             $data['sender_read']= "y";
             $data['receiver_read']= "y";
-            if($user[0]->userType == "Admin"){
+            if(isset($user[0]->isAdmin) && $user[0]->isAdmin == "yes"){
                 
                 $where = array();
                 $where['receiver_id = '] = "0";
@@ -131,27 +180,31 @@ class Messages extends CI_Controller {
         echo json_encode($status); exit;
     }
 
-    public function notification($reciever){
+    public function notification($recieverName, $recieverEmail){
        
-        $where = array("tempName"=>"EmailVerification");
+        $where = array("tempName"=>"msgNotification");
 		$emailContent = $this->CommonModel->getMasterDetails('emailMaster','',$where);
-		if(!isset($emailContent) || empty($emailContent))
+        //var_dump($emailContent);
+        if(!empty($emailContent)){
+            
+            $from= $this->config->item('supportEmail');
+            $fromName= "KPIT SHODH";  //$this->$this->fromName;
+
+            $subject=$emailContent[0]->subject;
+            $mailContent = str_replace("{{userName}}",$recieverName,$emailContent[0]->emailContent);
+
+            return $isEmailSend=$this->emails->sendMailDetails($from,$fromName,$recieverEmail,$cc='',$bcc='',$subject,$mailContent);
+        }
+		/*if(!isset($emailContent) || empty($emailContent))
 		{
 			$status['data'] = "";//"Email Template Not Found. 'Email Verification'";
 			$status['msg'] = "Email Template Not Found. 'Email Verification'";
 			$status['statusCode'] = 996;
 			$status['flag'] = 'F';
 			echo json_encode($status); exit;
-		}
-        $mailContent = str_replace("{{otp}}",$otp,$emailContent[0]->emailContent);
-		$mailContent = str_replace("{{userName}}",$name,$mailContent);
-		$from= $this->config->item('supportEmail');
-		$to=$email;
-		$subject=$emailContent[0]->subject;
-		$fromName= "KPIT SHODH";  //$this->$this->fromName;
-		$msg=$mailContent;
-        
-        return $isEmailSend=$this->emails->sendMailDetails($from,$fromName,$to,$cc='',$bcc='',$subject,$msg);
+		}*/
+        //$mailContent = str_replace("{{otp}}",$otp,$emailContent[0]->emailContent);
+		
     }
 }
 
